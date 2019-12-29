a8fc3b6b8ff52631c500395605c2b2388c8f8840
POWER <clay.degruchy@gmail.com>
2019-12-29 20:54:20 +0000

    removed that eval

---
+++ index.js
@@ -0,0 +1,6 @@
+var eco = require('./economia')
+
+eco.tick()
+eco.tick()
+eco.tick()
+console.log(eco)
--- src/Class.js
+++ src/Class.js
@@ -1,18 +1,4 @@
-// var createClass = function(className,methods) {   
-//     var ClassObj = function() {    
-//         this.initialize.apply(this, arguments);          
-//     };
-//     for (var property in methods) { 
-//        ClassObj.prototype[property] = methods[property];
-//     };
-    
-//     ClassObj.prototype._class = className;
 
-//     if (!ClassObj.prototype.initialize) ClassObj.prototype.initialize = function(){};      
-    
-//     return ClassObj;    
-// };
-//(function(){
   
 var createClass = function(className,methods) {   
     
